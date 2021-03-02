0f90708dec92b6d04ff0179d7f022b42a02b25f0
Mark McEver <mark@fastmail.net>
2021-03-02 18:12:26 +0000

    Improve log page load time

--- get-logs.php
+++ get-logs.php
@@ -9,19 +9,13 @@ $row = db_fetch_assoc($result);
 $totalRowCount = $row[$columnName];
 
 $results = $module->queryLogs("
-	select log_id, timestamp, message, details, failure
-	order by log_id desc
-	$limitClause
+select log_id, timestamp, message, failure, details = '' as hasDetails
+order by log_id desc
+$limitClause
 ");
 
 $rows = [];
 while($row = $results->fetch_assoc()){
-	$row['hasDetails'] = $row['details'] !== null;
-
-	// It's a little much for the browser to load all the details into memory.
-	// We now load them on the fly later instead.
-	unset($row['details']);
-
 	$rows[] = $row;
 }
 
