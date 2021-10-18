d8ccdd7b4996311615ca5d95b441973ff9ac5bf9
Mark McEver <mark@fastmail.net>
2021-10-18 17:11:47 -0500

    Verified SSL certs

--- APISyncExternalModule.php
+++ APISyncExternalModule.php
@@ -829,7 +829,7 @@ class APISyncExternalModule extends \ExternalModules\AbstractExternalModule{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		curl_setopt($ch, CURLOPT_VERBOSE, 0);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
