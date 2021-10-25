49958d716c1f6200643213430c1acfa9c852c82d
Mark McEver <mark@fastmail.net>
2021-10-25 21:47:28 +0100

    Prevented checking the same range of logs twice

--- APISyncExternalModule.php
+++ APISyncExternalModule.php
@@ -153,7 +153,7 @@ class APISyncExternalModule extends \ExternalModules\AbstractExternalModule{
 		return array_keys($dictionary);
 	}
 
-	private function getBatchesToSync(){
+	private function getBatchesToSync($latestLogId){
 		$batchBuilder = new BatchBuilder($this->getExportBatchSize());
 
 		if($this->getProjectSetting('export-all-records') === true){
@@ -161,7 +161,6 @@ class APISyncExternalModule extends \ExternalModules\AbstractExternalModule{
 
 			$recordIdFieldName = $this->getRecordIdField();
 			$records = json_decode(REDCap::getData($this->getProjectId(), 'json', null, $recordIdFieldName), true);
-			$latestLogId = $this->getLatestLogId();
 
 			foreach($records as $record){
 				// An empty fields array will cause all fields to be pulled.
@@ -233,100 +232,113 @@ class APISyncExternalModule extends \ExternalModules\AbstractExternalModule{
 			$excludedFieldNames = $this->getIdentifiers();
 		}
 
-		$batches = $this->getBatchesToSync();
-		for($i=0; $i<count($batches); $i++) {
-			$batch = $batches[$i];
-
-			$type = $batch->getType();
-			$lastLogId = $batch->getLastLogId();
-			$recordIds = $batch->getRecordIds();
-			$fieldsByRecord = $batch->getFieldsByRecord();
-
-			$batchText = "batch " . ($i+1) . " of " . count($batches);
-
-			$this->log("Preparing to export {$type}s for $batchText", [
-				'details' => json_encode([
-					'Last Log ID' => $lastLogId,
-					'Record IDs' => $recordIds
-				], JSON_PRETTY_PRINT)
-			]);
-
-			if($type === self::UPDATE){
-				$fields = $batch->getFields();
-
-				if(!empty($fields)){
-					$fields[] = $recordIdFieldName;
-				}
-
-				$data = json_decode(REDCap::getData(
-					$this->getProjectId(),
-					'json',
-					$recordIds,
-					$fields,
-					[],
-					[],
-					false,
-					false,
-					false,
-					false,
-					false,
-					false,
-					false,
-					$dateShiftDates
-				), true);
-
-				$subBatchData = [];
-				$subBatchSize = 0;
-				$subBatchNumber = 1;
-				for($rowIndex=0; $rowIndex<count($data); $rowIndex++){
-					$row = $data[$rowIndex];
-					foreach($batch->getFields() as $field){
-						if(!isset($fieldsByRecord[$row[$recordIdFieldName]][$field])){
-							// This field didn't change for this record, so don't include it in the export.
-							unset($row[$field]);
+		$latestLogId = $this->getLatestLogId();
+		$batches = $this->getBatchesToSync($latestLogId);
+		if(empty($batches)){
+			/**
+			 * No recent changes exist to sync.
+			 * Update the last exported log ID to whatever the latest ID across all projects is
+			 * in order to ensure that we don't check this date range again.
+			 * This can reduce query time significantly on projects thar are updated infrequently,
+			 * especially when the server is under heavy load.
+			 */
+			$this->setProjectSetting('last-exported-log-id', $latestLogId);
+		}
+		else{
+			for($i=0; $i<count($batches); $i++) {
+				$batch = $batches[$i];
+	
+				$type = $batch->getType();
+				$lastLogId = $batch->getLastLogId();
+				$recordIds = $batch->getRecordIds();
+				$fieldsByRecord = $batch->getFieldsByRecord();
+	
+				$batchText = "batch " . ($i+1) . " of " . count($batches);
+	
+				$this->log("Preparing to export {$type}s for $batchText", [
+					'details' => json_encode([
+						'Last Log ID' => $lastLogId,
+						'Record IDs' => $recordIds
+					], JSON_PRETTY_PRINT)
+				]);
+	
+				if($type === self::UPDATE){
+					$fields = $batch->getFields();
+	
+					if(!empty($fields)){
+						$fields[] = $recordIdFieldName;
+					}
+	
+					$data = json_decode(REDCap::getData(
+						$this->getProjectId(),
+						'json',
+						$recordIds,
+						$fields,
+						[],
+						[],
+						false,
+						false,
+						false,
+						false,
+						false,
+						false,
+						false,
+						$dateShiftDates
+					), true);
+	
+					$subBatchData = [];
+					$subBatchSize = 0;
+					$subBatchNumber = 1;
+					for($rowIndex=0; $rowIndex<count($data); $rowIndex++){
+						$row = $data[$rowIndex];
+						foreach($batch->getFields() as $field){
+							if(!isset($fieldsByRecord[$row[$recordIdFieldName]][$field])){
+								// This field didn't change for this record, so don't include it in the export.
+								unset($row[$field]);
+							}
+						}
+	
+						foreach($excludedFieldNames as $excludedFieldName){
+							unset($row[$excludedFieldName]);
+						}
+	
+						$rowSize = strlen(json_encode($row));
+	
+						$spaceLeftInSubBatch = $maxSubBatchSize - $subBatchSize;
+						if($rowSize > $spaceLeftInSubBatch){
+							if($subBatchSize === 0){
+								$this->log("The export failed because the sub-batch size setting is not large enough to handle the data in the details of this log message.", [
+									'details' => json_encode($row, JSON_PRETTY_PRINT)
+								]);
+	
+								throw new Exception("The export failed because of a sub-batch size error.  See the API Sync page for project " . $this->getProjectId() . " for details.");
+							}
+	
+							$this->exportSubBatch($servers, $type, $subBatchData, $subBatchNumber);
+							$subBatchData = [];
+							$subBatchSize = 0;
+							$subBatchNumber++;
+						}
+	
+						$subBatchData[] = $row;
+						$subBatchSize += $rowSize;
+	
+						$isLastRow = $rowIndex === count($data)-1;
+						if($isLastRow){
+							$this->exportSubBatch($servers, $type, $subBatchData, $subBatchNumber);
 						}
 					}
-
-					foreach($excludedFieldNames as $excludedFieldName){
-						unset($row[$excludedFieldName]);
-					}
-
-					$rowSize = strlen(json_encode($row));
-
-					$spaceLeftInSubBatch = $maxSubBatchSize - $subBatchSize;
-					if($rowSize > $spaceLeftInSubBatch){
-						if($subBatchSize === 0){
-							$this->log("The export failed because the sub-batch size setting is not large enough to handle the data in the details of this log message.", [
-								'details' => json_encode($row, JSON_PRETTY_PRINT)
-							]);
-
-							throw new Exception("The export failed because of a sub-batch size error.  See the API Sync page for project " . $this->getProjectId() . " for details.");
-						}
-
-						$this->exportSubBatch($servers, $type, $subBatchData, $subBatchNumber);
-						$subBatchData = [];
-						$subBatchSize = 0;
-						$subBatchNumber++;
-					}
-
-					$subBatchData[] = $row;
-					$subBatchSize += $rowSize;
-
-					$isLastRow = $rowIndex === count($data)-1;
-					if($isLastRow){
-						$this->exportSubBatch($servers, $type, $subBatchData, $subBatchNumber);
-					}
 				}
+				else if($type === self::DELETE){
+					$this->exportSubBatch($servers, $type, $recordIds, 1);
+				}
+				else{
+					throw new Exception("Unsupported export type: $type");
+				}
+	
+				$this->setProjectSetting('last-exported-log-id', $lastLogId);
+				$this->log("Finished exporting {$type}s for $batchText");
 			}
-			else if($type === self::DELETE){
-				$this->exportSubBatch($servers, $type, $recordIds, 1);
-			}
-			else{
-				throw new Exception("Unsupported export type: $type");
-			}
-
-			$this->setProjectSetting('last-exported-log-id', $lastLogId);
-			$this->log("Finished exporting {$type}s for $batchText");
 		}
 	}
 
